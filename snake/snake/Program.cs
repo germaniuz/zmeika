543566120334b5133b877e7e88a760da76d55d05
germaniuz <germanius@list.ru>
2015-09-25 02:06:15 +0300

    Create Lines

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -11,11 +11,17 @@ namespace snake
         static void Main(string[] args)
         {
 
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            // Point p1 = new Point(1, 3, '*');
+            // p1.Draw();
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            // Point p2 = new Point(4, 5, '#');
+            // p2.Draw();
+
+            HorizontalLine hLine1 = new HorizontalLine(5, 10, 8, '+');
+            hLine1.Drow();
+
+            VerticalLine vLine1 = new VerticalLine(6, 2, 8, '#');
+            vLine1.Drow();
 
             Console.ReadLine();
         }
