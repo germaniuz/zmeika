1119fba488a20f041c6a290ad8972949f6abaf78
germaniuz <germanius@list.ru>
2015-09-25 03:33:59 +0300

    Create Snake Class

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -24,18 +24,9 @@ namespace snake
             rightLine.Drow();
 
 
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
-
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
-
-            HorizontalLine hLine1 = new HorizontalLine(5, 10, 8, '+');
-            hLine1.Drow();
-
-            VerticalLine vLine1 = new VerticalLine(6, 2, 8, '#');
-            vLine1.Drow();
-
+            Point p = new Point(2, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
             Console.ReadLine();
         }
 
